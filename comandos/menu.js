503ed4448e92a364679266dd6eb9896e45997d39
werkito <zapoteco1234567@gmail.com>
2026-08-03 09:02:13 -0600

    Add menu command to send bot commands list

---
+++ comandos/menu.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "menu",
+  async execute(sock, m) {
+    const jid = m.key.remoteJid
+    const menu = `╭━━━〔 *TojiBot-WD* 〕━━━⬣
+┃ ✦ .ping
+┃ ✦ .menu
+┃ ✦ .sticker (responde a imagen)
+┃ ✦ .code 521xxx
+╰━━━━━━━━━━━━━━⬣
+> Bot hecho por Toji`
+    await sock.sendMessage(jid, { text: menu })
+  }
+}
