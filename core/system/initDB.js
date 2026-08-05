abf3fb4e4c878cff8271cfcbbf954357fa5f540e
werkito <zapoteco1234567@gmail.com>
2026-08-05 16:23:38 -0600

    Update initDB.js

--- core/system/initDB.js
+++ core/system/initDB.js
@@ -1,3 +1,5 @@
+cd ~/TojiBot-WD
+cat > core/system/initDB.js << 'EOF'
 let isNumber = (x) => typeof x === 'number' &&!isNaN(x)
 
 function initDB(m, client) {
@@ -7,12 +9,8 @@ function initDB(m, client) {
   settings.self??= false
   settings.prefix??= ['/', '!', '.', '#', '>']
   settings.commandsejecut??= isNumber(settings.commandsejecut)? settings.commandsejecut : 0
-
   settings.id??= '120363198641161536@newsletter'
-
-  // TojiBot-WD Canal Oficial
-  settings.nameid??= "'⚔️ Toji Fushiguro - TojiBot-WD Oficial ࿐"
-
+  settings.nameid??= '⚔️ Toji Fushiguro - TojiBot-WD Oficial'
   settings.type??= 'Owner'
   settings.link??= 'https://github.com/zapoteco1212/TojiBot-WD'
   settings.banner??= 'https://i.imgur.com/TojiFushiguroBanner.jpg'
@@ -25,4 +23,26 @@ function initDB(m, client) {
 
   const user = global.db.data.users[m.sender] ||= {}
   user.name??= m.pushName
-  user.exp = is
+  user.exp = isNumber(user.exp)? user.exp : 0
+  user.limit = isNumber(user.limit)? user.limit : 10
+  user.money = isNumber(user.money)? user.money : 0
+  user.joincount = isNumber(user.joincount)? user.joincount : 0
+  user.level = isNumber(user.level)? user.level : 0
+  user.role??= 'Novato'
+  user.registered??= false
+  user.banned??= false
+  user.premium??= false
+
+  const chat = global.db.data.chats[m.chat] ||= {}
+  chat.isBanned??= false
+  chat.welcome??= true
+  chat.antiLink??= false
+  chat.antiToxic??= false
+  chat.onlyAdmin??= false
+
+  return { settings, user, chat }
+}
+
+export default initDB;
+EOF
+node main.js
