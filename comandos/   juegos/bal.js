df3b0b76b38489474188b957baf4087abd22d928
werkito <zapoteco1234567@gmail.com>
2026-08-05 08:55:17 -0600

    Update bal.js

--- comandos/   juegos/bal.js
+++ comandos/   juegos/bal.js
@@ -4,10 +4,10 @@ export default {
   name: "bal",
   alias: ["billetera", "coins", "cartera", "wallet"],
   run: async (sock, msg, args) => {
-    const id = msg.key.remoteJid
+    const id = msg.key.participant || msg.key.remoteJid
     const user = getUser(id)
-    await sock.sendMessage(id, { 
-      text: `💰 *Billetera*\n\nTienes: *${user.coins}* TojiCoins` 
+    await sock.sendMessage(msg.key.remoteJid, {
+      text: `💰 *Billetera*\n\nTienes: *${user.coins}* TojiCoins`
     }, { quoted: msg })
   }
 }
