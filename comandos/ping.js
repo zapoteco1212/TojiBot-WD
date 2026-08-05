0459e57bdc07b6a3b9edf1cba45235ea6936bfc2
werkito <zapoteco1234567@gmail.com>
2026-08-05 12:40:27 -0600

    Delete comandos directory

--- comandos/ping.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: "ping",
-alias: ["p", "velocidad", "speed"],
-  async execute(sock, m, args) {
-    await sock.sendMessage(m.key.remoteJid, { text: "pong! 🏓" }, { quoted: m })
-  }
-}
