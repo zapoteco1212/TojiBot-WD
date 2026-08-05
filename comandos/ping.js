65045c71c4a0fef34d31bb275b71caaff6677e5f
werkito <zapoteco1234567@gmail.com>
2026-08-05 10:15:12 -0600

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,5 +1,6 @@
 export default {
   name: "ping",
+alias: ["p", "velocidad", "speed"],
   async execute(sock, m, args) {
     await sock.sendMessage(m.key.remoteJid, { text: "pong! 🏓" }, { quoted: m })
   }
