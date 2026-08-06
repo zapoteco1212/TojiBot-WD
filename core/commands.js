da31b9ba5822d10667fbfc039e154fa78b5738d8
werkito <zapoteco1234567@gmail.com>
2026-08-05 21:15:33 -0600

    Update commands.js

--- core/commands.js
+++ core/commands.js
@@ -1,1 +1,92 @@
+export const bodyMenu = `> 𖧧 ¡Hola! *@$sender*, Soy *$namebot*, Aquí tienes la lista de comandos$cat
 
+༺═━━━━━✦❖✦━━━━━═༻
+❖ *ᴅᴇᴠᴇʟᴏᴘᴇʀ ::* $owner
+✦ *ᴛʏᴘᴇ ::* $botType
+❖ *ᴠᴇʀsɪᴏɴ ::* ^3.0 - Toji Edition
+✦ *sʏsᴛᴇᴍ/ᴏᴘʀ ::* $device
+❖ *ᴛɪᴍᴇ ::* $tiempo, $tempo
+✦ *ᴜsᴇʀs ::* $users
+❖ *ᴜʀʟ ::* $link
+༺═━━━━━✦❖✦━━━━━═༻
+> Vincula un *Socket* con tu número utilizando *$prefixqr* o *$prefixcode*.
+‧꒷︶꒷꒥꒷‧₊˚꒷︶꒷꒥꒷︶꒷˚₊‧꒷꒥꒷︶꒷‧`
+
+export const menuObject = {
+economia: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐄𝐂𝐎𝐍𝐎𝐌𝐘 - Toji ⛩️ ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Economía.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+gacha: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐆𝐀𝐂𝐇𝐀 - Toji 🎴 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Gacha.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+rpg: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐑𝐏𝐆 - Toji ⚔️ ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de RPG.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+downloads: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐒 - Toji 📥 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Descargas.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+profile: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 - Toji 👤 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Perfil.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+sockets: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐒𝐎𝐂𝐊𝐄𝐓𝐒 - Toji 🤖 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Sub-Bots.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+stickers: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒 - Toji 🎨 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Stickers.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+utils: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐔𝐓𝐈𝐋𝐒 - Toji 🛠️ ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Utilidades.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+grupo: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐆𝐑𝐎𝐔𝐏𝐒 - Toji 👥 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Grupos.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+nsfw: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐍𝐒𝐅𝐖 - Toji 🔞 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos NSFW.
+
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+anime: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐀𝐍𝐈𝐌𝐄 - Toji 🌸 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Anime.
+
+༺═━━━━━✦❖✦━━━━━═༻`
+}
