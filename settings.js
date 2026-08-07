36543c8c02ae023b9c6069fb20ce131e6b8a9f38
werkito <zapoteco1234567@gmail.com>
2026-08-06 22:24:33 -0600

    Update bot settings and metadata in settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,14 @@
 global.owner = [
-  '527444200627', '526451030817', '526561814016'
+  '527444200627', '526451030817', '521234567890'
 ]
 global.mods = []
 global.prems = []
 global.botNumber = ''
-global.sessionName = './Sessions/Owner'
+global.sessionName = './session'
 global.botname = 'TojiBot-WD'
-global.namebot = 'TojiBot-WD'
+global.namebot = '⛩️ TojiBot-WD 🪱'
 global.wm = 'TojiBot-WD by zapoteco1212'
+global.vs = '2.0.0'
+global.packname = 'TojiBot-WD'
+global.author = 'zapoteco1212'
+global.wait = '⛩️ Cargando, gusano... ⏳'
